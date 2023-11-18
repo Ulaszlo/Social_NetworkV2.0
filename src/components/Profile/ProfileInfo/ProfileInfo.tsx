8a4fb4fb57820d9dd032a0cb92fb7a206decf2dc
Ulaszlo <cool.tsuranov@inbox.ru>
2023-11-18 22:47:48 +0200

    social_networkV2.0 + upload photo + css

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -4,6 +4,7 @@ import { TypeProfileDataType} from "../../../redux/Reducers/profile-reducer";
 import userAvatar from "../../../assets/imges/user-images.png"
 import React  from 'react';
 import { ProfileData } from '../ProfileData';
+import InputFileUpload from "../../common/Upload/UploadButton";
 type ProfileInfoType = {
     profile:TypeProfileDataType
     UserStatus: string
@@ -14,19 +15,13 @@ type ProfileInfoType = {
     savePhoto:(userPhoto:any)=>void
 }
 export const ProfileInfo = (props: ProfileInfoType) => {
-    const onChangeFile=(e:any)=>{
 
-        if (e.target.files.length) {
-            props.savePhoto(e.target.files[0])
-        }
-
-    }
 
 
     return (
         <div className={s.profileInfoContent}>
             <img className={s.profileUserPhoto} src={props.profile.photos.large || userAvatar }/>
-          <div>{props.isOwner && <input onChange={onChangeFile} type='file'/>}</div>
+          <div>{props.isOwner && <InputFileUpload  savePhoto={props.savePhoto}   />}</div>
             <span className={s.userName}>{props.profile.fullName}</span>
             <div title="изменить статус"><ProfileStatusFC UserStatus={props.UserStatus} updateUserStatus={props.updateUserStatus}/></div>
             <ProfileData profile={props.profile}/>
---
+++ src/components/common/Upload/UploadButton.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import uploadIcon from '../../../assets/imges/upload-icon-6.png'
+import style from './Upload.module.css'
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+export default function  InputFileUpload(props:any) {
+    const onChangeFile=(e:any)=>{
+        debugger;
+        if (e.target.files.length) {
+            props.savePhoto(e.target.files[0])
+        }
+
+    }
+
+    return (
+        <Button component="label" variant="contained" startIcon={<img className={style.uploadIcon} src={uploadIcon}/>} >
+            Upload file
+            <VisuallyHiddenInput type="file" onChange={onChangeFile} />
+        </Button>
+    );
+}
